40b719bee70d633654f531c5ecd22c8fd16954f3
Andrew Longhorn <Andrew_Longhorn@TrendMicro.com>
2022-07-21 09:43:07 +1000

    first release

--- workloadsecurity/listcomputers.js
+++ workloadsecurity/listcomputers.js
@@ -8,11 +8,20 @@ module.exports = function(RED) {
         const streaming = false;    // todo: future streaming option
  
         node.on('input', function(msg) {
+            const expand = ['applicationControl','none'];
+            const expands = 'expand=' + expand.join('&expand=');
+            const overrides = '&overrides=false';
+            console.log('https://workload.{{region}}.cloudone.trendmicro.com/api/computers?' + expands + overrides);
             node._cloudone.call(node, {
                 method: "GET",
-                uri: "https://workload.{{region}}.cloudone.trendmicro.com/api/computers?expand=applicationControl&expand=none&overrides=false",
+                uri: {
+                    service: 'workload',
+                    region: (msg && msg.payload && msg.payload.region) || undefined,
+                    path: 'computers?' + expands + overrides
+                },
                 headers: {
-                    'Api-Version': 'v1'
+                    'Api-Version': 'v1',
+                    //'Content-Type': 'application/vnd.api+json'
                 }
             });
         }, null, function(body) {
