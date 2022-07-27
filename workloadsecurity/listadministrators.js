47ac89dba85f7dcd093b6c85bc2ba298c90fec4d
Andrew Longhorn <Andrew_Longhorn@TrendMicro.com>
2022-07-27 11:53:07 +1000

    Added get all administrators fixed documentation for get all computers added parameters for get all computers

--- workloadsecurity/listadministrators.js
+++ workloadsecurity/listadministrators.js
@@ -1,6 +1,6 @@
 module.exports = function(RED) {
 
-    function ListComputers(config) {
+    function ListAdministrators(config) {
         RED.nodes.createNode(this, config);
         const node = this;
 
@@ -8,17 +8,12 @@ module.exports = function(RED) {
         const streaming = false;    // todo: future streaming option
  
         node.on('input', function(msg) {
-            // todo: better parameter checking, msg.payload needs to be an array of strings
-            const expand = config.expand || msg.payload.expand || [];
-            const expands = 'expand=' + (expand.length > 0 ? expand.join('&expand=') : 'none');
-            const overrides = '&overrides=' + (msg.overrides !== undefined ? msg.payload.overrides : config.overrides === true);
-            console.log('https://workload.{{region}}.cloudone.trendmicro.com/api/computers?' + expands + overrides);
             node._cloudone.call(node, {
                 method: "GET",
                 uri: {
                     service: 'workload',
                     region: (msg && msg.payload && msg.payload.region) || undefined,
-                    path: 'computers?' + expands + overrides
+                    path: 'administrators'
                 },
                 headers: {
                     'Api-Version': 'v1',
@@ -26,23 +21,23 @@ module.exports = function(RED) {
                 }
             }, null, function(body) {
                 console.log(body);
-                if (!body || !body.computers) {
-                    node.status({ fill:"red", shape:"ring", text:"Response Error, Missing 'computers'." });
+                if (!body || !body.administrators) {
+                    node.status({ fill:"red", shape:"ring", text:"Response Error, Missing 'administrators'." });
                     return;
                 }
-                const count = body.computers.length;
-                node.status({ fill:"green", shape:"dot", text:"Listed " + count + ' computer' + (count === 1 ? '' : 's')});
+                const count = body.administrators.length;
+                node.status({ fill:"green", shape:"dot", text:"Listed " + count + ' administrator' + (count === 1 ? '' : 's')});
                 if (streaming) {
                     // foreach (computers) {
                     //     node.send([computer]);
                     // }
                 } else {
                     node.send({
-                        payload: body.computers
+                        payload: body.administrators
                     });
                 }
             });
         });
     }
-    RED.nodes.registerType("list computers", ListComputers);
+    RED.nodes.registerType("list administrators", ListAdministrators);
 };
